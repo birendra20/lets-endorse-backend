87abda38a6098734529654fcc86df49569fd98ea
birendra20 <birendragupta20@gmail.com>
2022-06-23 18:58:11 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const employeeController = require("./controllers/employee.controller");
 
 const connect = require("./configs/db");
-
+const port = process.env.PORT || 2345;
 const app = express();
 
 app.use(express.json());
 
 app.use("/employees", employeeController);
 
-app.listen(2345, async function () {
+app.listen(port, async function () {
   try {
     await connect();
 
