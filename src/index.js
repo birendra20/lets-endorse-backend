3b2e0e21ff142a5b8551f8057d4319d7bbb0be59
birendra20 <birendragupta20@gmail.com>
2022-06-23 13:35:13 +0530

    db con..

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
+const connect = require("./configs/db");
+
 const app = express();
 
-app.listen(2345, function () {
-  console.log("listening to port 2345");
+app.listen(2345, async function () {
+  try {
+    await connect();
+
+    console.log("listening to port 2345");
+  } catch (error) {
+    console.log("error is ", error.message);
+  }
 });
