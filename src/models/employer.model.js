f10609c7d019a6da008c6ec7ed86b3b32a85a6da
birendra20 <birendragupta20@gmail.com>
2022-06-27 16:42:15 +0530

    .

--- src/models/employer.model.js
+++ src/models/employer.model.js
@@ -1,37 +1,6 @@
 const mongoose = require("mongoose");
 
 const employeeSchema = new mongoose.Schema(
-  //   {
-  //     basic: {
-  //       address: {
-  //         line2: { type: String, required: true },
-  //         city: { type: String, required: true },
-  //         district: { type: String, required: true },
-  //         state: { type: String, required: true },
-  //         line1: { type: String, required: true },
-  //         country: { type: String, required: true },
-  //       },
-  //       employer: { type: String, required: true },
-  //       altMobileNumber: { type: String, required: true },
-  //       identityType: { type: Array, required: true },
-  //       source: { type: String, required: true },
-  //       dob: { type: Date, required: true },
-  //       gender: { type: String, required: true },
-  //       educationalQualification: { type: String, required: true },
-  //       specializationOfEducation: { type: String, required: true },
-  //       religion: { type: String, required: true },
-  //       category: { type: String, required: true },
-  //       employer: { type: String, required: true },
-  //       employmentStatus: { type: String, required: true },
-  //       currentAnnualIncome: { type: String, required: true },
-  //       experience: { type: String, required: true },
-  //       mobileNumber: { type: Number, required: true },
-  //       name: { type: String, required: true },
-  //       phase: { type: String, required: true },
-  //       pincode: { type: Number, required: true },
-  //     },
-
-  //   }
   {
     id: {
       type: String,
